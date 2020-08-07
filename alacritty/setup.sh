9560daee8d04dc32f8fbdfc3459615978a17101e
Pascal Kuendig <padakuro@gmail.com>
2020-08-07 11:36:31 +0200

    alacritty: import

---
+++ alacritty/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+if [ ${#DOTFILES_LIB_ROOT} -lt 5 ]; then echo "No no no."; exit 1; fi; source "${DOTFILES_LIB_ROOT}/index"
+
+ensure_arch_package "alacritty"
+
+install_directory "${DOTFILES_SELF_ROOT}/.config"
