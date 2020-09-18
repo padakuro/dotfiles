fb0d5ad2dd533ba7758ee1e7719509d76b97b83d
Pascal Kuendig <padakuro@gmail.com>
2020-09-18 08:09:38 +0200

    gnome: add paperwm config

--- gnome/setup.sh
+++ gnome/setup.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env zsh
 if [ ${#DOTFILES_LIB_ROOT} -lt 5 ]; then echo "No no no."; exit 1; fi; source "${DOTFILES_LIB_ROOT}/index"
 
+confPaths=(
+  org/gnome/settings-daemon/plugins/media-keys
+
+  org/gnome/mutter
+
+  org/gnome/shell/keybindings
+  org/gnome/shell/extensions/paperwm
+  org/gnome/shell/extensions/dash-to-panel
+
+  org/gnome/desktop/media-handling
+  org/gnome/desktop/datetime
+  org/gnome/desktop/calendar
+  org/gnome/desktop/interface
+  org/gnome/desktop/lockdown
+  org/gnome/desktop/peripherals/touchpad
+  org/gnome/desktop/input-sources
+  org/gnome/desktop/wm/keybindings
+
+  org/gnome/terminal/legacy
+)
+
 case "$1" in
   save)
-    dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > ${DOTFILES_SELF_ROOT}/media-keys.conf
-    dconf dump /org/gnome/shell/ > ${DOTFILES_SELF_ROOT}/shell.conf
-    dconf dump /org/gnome/desktop/ > ${DOTFILES_SELF_ROOT}/desktop.conf
-    dconf dump /org/gnome/terminal/legacy/ > ${DOTFILES_SELF_ROOT}/terminal.conf
+    for cfg in "${confPaths[@]}"; do
+      fileName="${cfg//\//-}.conf"
+      dconf dump "/${cfg}/" > "${DOTFILES_SELF_ROOT}/${fileName}"
+    done
   ;;
 
   *)
-    dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ${DOTFILES_SELF_ROOT}/media-keys.conf
-    dconf load /org/gnome/shell/ < ${DOTFILES_SELF_ROOT}/shell.conf
-    dconf load /org/gnome/desktop/ < ${DOTFILES_SELF_ROOT}/desktop.conf
-    dconf load /org/gnome/terminal/legacy/ < ${DOTFILES_SELF_ROOT}/terminal.conf
+    for cfg in "${confPaths[@]}"; do
+      fileName="${cfg//\//-}.conf"
+      dconf load "/${cfg}/" < "${DOTFILES_SELF_ROOT}/${fileName}"
+    done
 
     install_directory "${DOTFILES_SELF_ROOT}/.config"
     install_directory "${DOTFILES_SELF_ROOT}/.zsh"
